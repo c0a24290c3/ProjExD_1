a590bdb69ff2b0f5d3f3599663911b6002847b68
c0a24290 <c0a24290c3@edu.teu.ac.jp>
2025-04-15 16:32:40 +0900

    練習10　こうかとんを動かす

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,29 @@ def main():
     kk_img = pg.image.load("fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_rct = kk_img.get_rect()#練習10-1
+    kk_rct.center = 300, 200#練習10-2
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
+        key_lst = pg.key.get_pressed()#練習10-3
+        if key_lst[pg.K_UP]:#練習10-4
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
+        
 
         X = tmr%3200 #練習6,9
         screen.blit(bg_img, [-X, 0])
         screen.blit(bg_img2, [-X+1600,0])#練習7,8
         screen.blit(bg_img, [-X+3200,0])#練習9
-        screen.blit(kk_img, [300, 200]) #練習4
+        screen.blit(kk_img, kk_rct) #練習4,10-5
         pg.display.update()
         tmr += 1        
         clock.tick(200) #練習5
